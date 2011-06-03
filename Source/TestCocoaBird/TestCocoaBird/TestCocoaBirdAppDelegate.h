1104e79042efe7d0df6b0ef190c39f495f53bb5c
Josh Wright <jwright@mtmrecognition.com>
2011-06-03 17:20:17 -0500

    added basic project

---
+++ Source/TestCocoaBird/TestCocoaBird/TestCocoaBirdAppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  TestCocoaBirdAppDelegate.h
+//  TestCocoaBird
+//
+//  Created by JOSHUA WRIGHT on 6/3/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TestCocoaBirdAppDelegate : NSObject <UIApplicationDelegate> {
+
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
